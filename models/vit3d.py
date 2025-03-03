388b9b2c7d1935f1fb92c106be24a536b05942dd
sarathir-dev <sarathir.dev@gmail.com>
2025-03-03 09:32:49 +0530

    Implement base model architecture

--- models/vit3d.py
+++ models/vit3d.py
@@ -1,1 +1,23 @@
 # Vision Transformer model (ViT for 3D)
+
+import torch
+import torch.nn.functional as F
+from torch import nn
+from einops import rearrange
+from einops.layers.torch import Rearrange
+
+
+class FeedForward(nn.module):
+    return
+
+
+class Attention(nn.module):
+    return
+
+
+class Transformer(nn.module):
+    return
+
+
+class SimpleViT(nn.module):
+    return
